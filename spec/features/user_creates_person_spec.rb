39d8b3016b023aeeac8f66b120c13272bfa54f5e
edensongjbs <edensongjbs@gmail.com>
2020-10-26 12:52:19 -0400

    test work

--- spec/features/user_creates_person_spec.rb
+++ spec/features/user_creates_person_spec.rb
@@ -1,7 +1,23 @@
 require "rails_helper"
 
 feature "user creates person" do
+
   scenario "with valid data" do
-    pending "Use Capybara to create a new person"
+    visit new_person_url
+
+    fill_in "First name", with: "James"
+    click_button "Create Person"
+
+    expect(page).to have_content("Person created.")
+    expect(page).to have_content("James")
+  end
+
+  scenario "with missing data" do
+    visit new_person_url
+
+    fill_in "First name", with: ""
+    click_button "Create Person"
+
+    expect(page).to have_content("can't be blank")
   end
 end
