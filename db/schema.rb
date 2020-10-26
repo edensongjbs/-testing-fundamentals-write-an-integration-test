98491d9f774dad36421680cede6c98dac00f80f3
edensongjbs <edensongjbs@gmail.com>
2020-10-26 12:46:31 -0400

    fix dependencies

--- db/schema.rb
+++ db/schema.rb
@@ -1,19 +1,18 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140930151331) do
+ActiveRecord::Schema.define(version: 2014_09_30_151331) do
 
-  create_table "people", force: true do |t|
+  create_table "people", force: :cascade do |t|
     t.string "first_name"
   end
 
